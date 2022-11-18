6dc0fa1c07815c114ca0f34c8d0daff58d424d0d
sathishkumar5661 <113517532+sathishkumar5661@users.noreply.github.com>
2022-11-18 07:25:58 -0800

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/SPRINT 2/IBMDB2.py
@@ -0,0 +1,36 @@
+import ibm_db
+import pandas
+import ibm_db_dbi
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite://',
+                       echo = False)
+
+dsn_hostname = "9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "brw34611"
+dsn_pwd = "mt5aNZsTSa4yDGuw"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"
+dsn_port = "32459"
+dsn_protocol = "TCPIP"
+dsn_security = "SSL"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
